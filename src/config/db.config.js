c5c885d627bfb02f890b55f7eb80ec89a48aeb8c
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2023-06-02 15:00:26 +0700

    feat: add config database

---
+++ src/config/db.config.js
@@ -0,0 +1,27 @@
+const { development, test, production } = require("./config.js");
+const env = process.env.NODE_ENV || "development";
+let config = {
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+if (env === "production") {
+  config = {
+    ...config,
+    ...production,
+  };
+} else if (env === "development") {
+  config = {
+    ...config,
+    ...development,
+  };
+} else if (env === "test") {
+  config = {
+    ...config,
+    ...test,
+  };
+}
+module.exports = config;
